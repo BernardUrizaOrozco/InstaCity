12f4d792619cba9c63607622cb1c65ba72b205b5
BernardUrizaOrozco <bernard.uriza@workteam.com.mx>
2021-04-01 18:27:29 -0600

    image in nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,6 +19,11 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            
+                            <div class="rounded-full h-8 w-8 mr-2 bg-gray-500 flex items-center overflow-hidden">
+                                <img src="/images/{{ Auth::user()->profile->image }}" alt="profilepic">
+                            </div>
+                            
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
