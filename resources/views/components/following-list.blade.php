eef5c0e6be05d644a4f88340261e1e42d8a7cff6
BernardUrizaOrozco <bernard.uriza@workteam.com.mx>
2021-04-01 18:24:00 -0600

    following list, mejora

--- resources/views/components/following-list.blade.php
+++ resources/views/components/following-list.blade.php
@@ -11,14 +11,15 @@
                 <img src="/images/{{ $following->profile->image }}" alt="profilepic">
             </div>
             <div class="ml-16 text-lg leading-6 font-medium text-gray-900">
-                <!-- follow button -->
-                <form method="POST" action="{{ route('follow') }}" enctype="multipart/form-data">
+                <!-- follow button 
+                <form method="POST" class="d-none" name="form{{ $following->profile->id }}" action="{{ route('follow') }}" enctype="multipart/form-data">
                     @csrf
-                    <input type="hidden" name="follow" id="follow" value="{{ $following->id }}" />
+                    <input type="hidden" name="follow" value="{{ $following->profile->id }}" />
                     <button type="submit" class="bg-blue-500 px-2 py-1 
                         text-white font-semibold text-sm rounded text-center 
                         sm:inline-block block">Follow</button>
-                    <form>
+                    <form>-->
+                <a href="/show/{{ $following->username }}">{{ "@".$following->username }}</a>
             </div>
         </dt>
         <dd class="mt-2 ml-16 text-base text-gray-500">
